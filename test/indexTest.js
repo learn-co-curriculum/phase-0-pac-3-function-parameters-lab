61bd9e1635939493628ebdddf2f5c270f91f9dbd
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-12-07 13:46:14 -0800

    add require to test file

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,5 @@
+require ( './root.js' );
+
 
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
